908e51e6be6349ec4cc90151ca3d6ad11bc0181f
oxalica <oxalicc@pm.me>
2020-03-21 20:35:48 +0800

    Remove opencv test and correct dev env

--- default.nix
+++ default.nix
@@ -2,8 +2,8 @@ with import <nixpkgs> {};
 mkShell {
   buildInputs = [
     pkg-config
-    (opencv4.override { enableGtk3 = true; })
     gtk3
     opencl-icd
+    cargo
   ];
 }
--- src/bin/test_opencv.rs
+++
@@ -1,8 +0,0 @@
-use opencv::{highgui::*, imgcodecs::*};
-
-fn main() -> Result<(), failure::Error> {
-    let mat = imread("assets/ddg.png", IMREAD_COLOR)?;
-    imshow("test_opencv", &mat)?;
-    wait_key(0)?;
-    Ok(())
-}
